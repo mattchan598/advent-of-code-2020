850180d6429578117538e3ee74e56b66d6d54149
Matt Chan <chan.598@osu.edu>
2020-12-05 00:52:40 -0500

    Add function to parse input

--- day01a.py
+++ day01a.py
@@ -1,7 +1,12 @@
 INPUT_FILE = "input01.txt"
 
-def main():
-    print("Hello Word")
+def part_1():
+    input = get_nums(INPUT_FILE)
+    print(sorted(input))
+
+def get_nums(input_file):
+    with open(input_file) as f:
+        return [int(line.strip()) for line in f.readlines()]
 
 if __name__ == '__main__':
-    main()
+    part_1()
