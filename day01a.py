4c804e68f0d7785925f7374ab0c4e29b9aaf677b
Matt Chan <chan.598@osu.edu>
2020-12-05 14:01:51 -0500

    First pass at day01a

--- day01a.py
+++ day01a.py
@@ -1,8 +1,18 @@
 INPUT_FILE = "input01.txt"
+SUM = 2020
 
 def part_1():
-    input = get_nums(INPUT_FILE)
-    print(sorted(input))
+    input = sorted(get_nums(INPUT_FILE), reverse=True)
+    # print(type(input))
+    for num_1 in input:
+        other_num = 2020 - num_1
+        for num_2 in reversed(input):
+            if num_2 > other_num:
+              break
+            elif num_2 == other_num:
+                print(f'{num_1} + {num_2} = {SUM}')
+                print(f'Answer is {num_1} * {num_2} = {num_1 * num_2}')
+
 
 def get_nums(input_file):
     with open(input_file) as f:
