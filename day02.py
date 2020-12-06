9d6724d24e5960c71292b7d391836e10d0414fbe
Matt Chan <chan.598@osu.edu>
2020-12-06 14:23:00 -0500

    Part 1: fin

--- day02.py
+++ day02.py
@@ -1,14 +1,19 @@
 import re
 
 INPUT_FILE = "input02.txt"
-line_regex = r'(\d+)-(\d+) ([a-z]): ([a-z]+)'
-count = 0
+line_regex = r"(\d+)-(\d+) ([a-z]): ([a-z]+)"
 
 def part_1():
+    count = 0
     input = get_input(INPUT_FILE)
     test = re.compile(line_regex)
     for line in input:
+        num_lines += 1
         (min_num, max_num, letter, password) = test.match(line).groups()
+        occurrence = password.count(letter)
+        if occurrence >= int(min_num) and occurrence <= int(max_num):
+            count += 1
+    print(f"Number of valid passwords: {count}")
 
 def part_2():
     print("Part 2")
