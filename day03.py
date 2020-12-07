fd4e537c8f914c72851d0b414895351b9803c78a
Matt Chan <chan.598@osu.edu>
2020-12-07 18:12:57 -0500

    Day 3: Add input file and skeleton file

---
+++ day03.py
@@ -0,0 +1,15 @@
+INPUT_FILE = "input02.txt"
+
+def part_1():
+    count = 0
+
+def part_2():
+    count = 0
+
+def get_input(input_file):
+    with open(input_file) as f:
+        return [line.strip() for line in f.readlines()]
+
+if __name__ == '__main__':
+    part_1()
+    part_2()
